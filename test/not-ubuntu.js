3a72273dc3f2baad2a82f7609a176a0fc95c4b4a
mh-cbon <mh-cbon@users.noreply.github.com>
2016-04-13 16:55:33 +0200

    fix tests

--- test/not-ubuntu.js
+++ test/not-ubuntu.js
@@ -3,8 +3,8 @@ var should = require('should')
 describe('hasUpstart', function() {
   it('returns false on upstart-friendly system', function(done) {
     require('../index.js')(function (err, initctlPath) {
-      (err===undefined).should.be.false;
-      (initctlPath===undefined).should.be.true;
+      (err===undefined).should.eql(false);
+      (initctlPath===undefined).should.eql(true);
       done();
     })
   });
--- test/ubuntu.js
+++ test/ubuntu.js
@@ -3,7 +3,7 @@ var should = require('should')
 describe('hasUpstart', function() {
   it('returns true on upstart-friendly system', function(done) {
     require('../index.js')(function (err, initctlPath) {
-      (err===undefined).should.be.false;
+      (err===undefined).should.eql(true);
       (initctlPath).should.match(/initctl$/);
       done();
     })
